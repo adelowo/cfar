f6a40aa584ad25c1df66d3bd0aac89036e813885
Lanre Adelowo <me@adelowolanre.com>
2016-07-13 19:32:52 +0100

    Tests not written yet ....

--- tests/CfarTest.php
+++ tests/CfarTest.php
@@ -5,98 +5,17 @@ namespace adelowo\cfar;
 require_once 'fixtures/GlobalController.php';
 require_once 'fixtures/HomeController.php';
 
-use adelowo\cfar\Cfar;
-use Aura\Router\RouterFactory;
+use adelowo\controller\HomeController;
+use Aura\Router\Map;
+use Aura\Router\Matcher;
+use Aura\Router\Route;
+use Aura\Router\RouterContainer;
+use Zend\Diactoros\ServerRequestFactory;
 
 class CfarTest extends \PHPUnit_Framework_TestCase
 {
-    protected $cfarInstance ;
+	public function setUp()
+	{
 
-    protected $auraRouterInstance ;
-
-    protected $server ;
-
-    public function setUp()
-    {
-        $this->auraRouterInstance = $this->getRouter();
-
-        $this->server = array('REQUEST_METHOD' => 'GET');
-
-
-        $this->cfarInstance = new Cfar($this->auraRouterInstance);
-    }
-
-    protected function getRouter()
-    {
-        $auraRouter = new RouterFactory();
-
-        return $auraRouter->newInstance();
-    }
-
-
-    public function testRouteParameters()
-    {
-        $this->auraRouterInstance->addGet(null , '/{id}/{name}')
-                ->addValues([
-                    "cfar" => [
-                        "namespace" => "adelowo\\controller\\",
-                        "controller" => "HomeController@showUser"
-                    ]
-                ]);
-
-        $route = $this->auraRouterInstance->match("/10/adelowo" , $this->server);
-
-        $expectedParameters = [
-            "id" => "10", //params are strings
-            "name" => "adelowo"
-        ];
-
-        $this->cfarInstance->dispatch();
-
-        $this->assertEquals($expectedParameters , $this->cfarInstance->getParameters());
-
-        $this->assertSame($expectedParameters , $this->cfarInstance->getParameters());
-
-    }
-
-    public function testNameSpacedController()
-    {
-        $this->auraRouterInstance->addGet(null , '/{id}/{name}')
-                ->addValues([
-                    "cfar" => [
-                        "namespace" => "adelowo\\controller\\",
-                        "controller" => "HomeController@showUser"
-                    ]
-                ]);
-
-        $route = $this->auraRouterInstance->match("/10/adelowo" , $this->server);
-
-        $this->cfarInstance->dispatch();
-
-        $controller = $this->cfarInstance->getController();
-
-        $this->assertInstanceOf("adelowo\\controller\\HomeController" ,
-         new $controller);
-
-    }
-
-    public function testControllerInGlobalNameSpace()
-    {
-        $this->auraRouterInstance->addGet(null , '/{id}/{name}')
-                ->addValues([
-                    "cfar" => [
-                        "controller" => "GlobalController@indexAction"
-                    ]
-                ]);
-
-        $route = $this->auraRouterInstance->match("/10/adelowo" , $this->server);
-
-        $this->cfarInstance->dispatch();
-
-        $controller = $this->cfarInstance->getController();
-
-        $this->assertInstanceOf("\\GlobalController" ,
-         new $controller);
-
-    }
+	}
 }

ffda3e2270c5fce2cf80c39490da617814eb439e
Lanre Adelowo <me@adelowolanre.com>
2016-07-18 12:33:50 +0100

    Made the package PSR Compliant, made check for the `listener` key more explicit, hence cfar can easily decipher to call the default method

--- cfar/src/Cfar.php
+++ cfar/src/Cfar.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace adelowo\cfar;
+namespace Adelowo\Cfar;
 
 use Aura\Router\Route;
 
@@ -13,129 +13,130 @@ use Aura\Router\Route;
 class Cfar
 {
 
-	/**
-	 * This is here to bail the current request if a "listener" was not defined on the class.
-	 * @var string
-	 */
-	const CFAR_DEFAULT_METHOD = 'indexAction';
+    /**
+     * This is here to bail the current request if a "listener" was not defined on the class.
+     * @var string
+     */
+    const CFAR_DEFAULT_METHOD = 'indexAction';
 
-	/**
-	 * Aura.Router Instance
-	 * @var Route
-	 */
-	protected $matchedRoute;
+    /**
+     * Aura.Router Instance
+     * @var Route
+     */
+    protected $matchedRoute;
 
-	/**
-	 * The name of the called controller class (plus it's fully quantified namespace).
-	 * @var string
-	 */
-	protected $controller;
+    /**
+     * The name of the called controller class (plus it's fully quantified namespace).
+     * @var string
+     */
+    protected $controller;
 
-	/**
-	 * The name of the method invoked on the called class .
-	 * @var string
-	 */
-	protected $method;
+    /**
+     * The name of the method invoked on the called class .
+     * @var string
+     */
+    protected $method;
 
-	/**
-	 * The parameters to be passed unto the method.
-	 * @var array
-	 */
-	protected $parameters;
+    /**
+     * The parameters to be passed unto the method.
+     * @var array
+     */
+    protected $parameters;
 
-	/**
-	 * @param Route $router
-	 */
-	public function __construct(Route $router)
-	{
-		$this->matchedRoute = $router;
-	}
+    /**
+     * @param Route $router
+     */
+    public function __construct(Route $router)
+    {
+        $this->matchedRoute = $router;
+    }
 
-	/**
-	 * Returns an array of parameters for the matched route.
-	 * @return array
-	 */
-	public function getParameters()
-	{
-		return $this->parameters;
-	}
+    /**
+     * Returns an array of parameters for the matched route.
+     * @return array
+     */
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
 
-	/**
-	 * Returns a string that denotes the called class plus it's namespace
-	 * @return string
-	 */
-	public function getController()
-	{
-		return $this->controller;
-	}
+    /**
+     * Returns a string that denotes the called class plus it's namespace
+     * @return string
+     */
+    public function getController()
+    {
+        return $this->controller;
+    }
 
-	/**
-	 * Returns a string that denotes the method that was invoked.
-	 * @return string
-	 */
-	public function getMethod()
-	{
-		return $this->method;
-	}
+    /**
+     * Returns a string that denotes the method that was invoked.
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
 
-	/**
-	 * Gets information about the method that is to be invoked.
-	 * @return \ReflectionMethod
-	 */
-	protected function getMethodToInvoke()
-	{
-		$reflection = new \ReflectionClass($this->controller);
+    /**
+     * Gets information about the method that is to be invoked.
+     * @return \ReflectionMethod
+     */
+    protected function getMethodToInvoke()
+    {
+        $reflection = new \ReflectionClass($this->controller);
 
-		return $reflection->getMethod($this->method);
-	}
+        return $reflection->getMethod($this->method);
+    }
 
-	public function dispatch()
-	{
-		$this->doesRouteHaveAValidDeclaration();
+    public function dispatch()
+    {
+        $this->doesRouteHaveAValidDeclaration();
 
-		list($this->controller, $this->method) = $this->getRegisteredControllerAndMethod();
+        list($this->controller, $this->method) = $this->getRegisteredControllerAndMethod();
 
-		$this->parameters = $this->matchedRoute->attributes;
+        $this->parameters = $this->matchedRoute->attributes;
 
-		$this->getReflectionClass($this->controller)
-			->getMethod($this->method)
-			->invokeArgs(new $this->controller, $this->parameters);
+        $this->getReflectionClass($this->controller)
+            ->getMethod($this->method)
+            ->invokeArgs(new $this->controller, $this->parameters);
 
-	}
+    }
 
-	/**
-	 * @return array
-	 */
-	protected function getRegisteredControllerAndMethod()
-	{
-		return [
-			$this->matchedRoute->handler,
-			($this->matchedRoute->extras['listener'] ?: self::CFAR_DEFAULT_METHOD)
-		];
-	}
+    /**
+     * @return array
+     */
+    protected function getRegisteredControllerAndMethod()
+    {
+        return [
+            $this->matchedRoute->handler,
+            array_key_exists('listener',
+                $this->matchedRoute->extras) ? $this->matchedRoute->extras['listener'] : self::CFAR_DEFAULT_METHOD
+        ];
+    }
 
-	/**
-	 * @return bool
-	 * @throws \adelowo\cfar\CfarException
-	 */
-	protected function doesRouteHaveAValidDeclaration()
-	{
-		$handler = $this->matchedRoute->handler;
+    /**
+     * @return bool
+     * @throws \adelowo\cfar\CfarException
+     */
+    protected function doesRouteHaveAValidDeclaration()
+    {
+        $handler = $this->matchedRoute->handler;
 
-		if ($validClass = $this->getReflectionClass($handler)) {
-			return true;
-		}
+        if ($validClass = $this->getReflectionClass($handler)) {
+            return true;
+        }
 
-		throw new CfarException("Invalid Route Declaration");
-	}
+        throw new CfarException("Invalid Route Declaration");
+    }
 
 
-	/**
-	 * @param $class string|object The class to resolve
-	 * @return \ReflectionClass
-	 */
-	protected function getReflectionClass($class)
-	{
-		return new \ReflectionClass($class);
-	}
+    /**
+     * @param $class string|object The class to resolve
+     * @return \ReflectionClass
+     */
+    protected function getReflectionClass($class)
+    {
+        return new \ReflectionClass($class);
+    }
 }

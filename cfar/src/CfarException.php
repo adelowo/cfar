e0c5d3a297f1d3399f40c1c9015ac2d382a6a2cd
Lanre Adelowo <me@adelowolanre.com>
2016-07-23 17:04:33 +0100

    Error message implementation with `__toString`

--- cfar/src/CfarException.php
+++ cfar/src/CfarException.php
@@ -3,9 +3,22 @@
 namespace Adelowo\Cfar;
 
 /**
- * @author Adelowo Lanre
+ * @author Lanre Adelowo
  */
 class CfarException extends \Exception
 {
 
+    protected $message;
+
+    const INVALID_DECLARATION = "Invalid route declaration";
+
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    public function __toString()
+    {
+        return self::INVALID_DECLARATION . ". The " . $this->getMessage();
+    }
 }
